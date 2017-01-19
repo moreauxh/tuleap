f2ce220fdbc9e10901c398e8271559891110525c
Humbert <humbert.moreaux@enalean.com>
2017-01-19 16:03:43 +0100

    insert json in json with double quotes

--- actions/add_artifact.sh
+++ actions/add_artifact.sh
@@ -2,5 +2,5 @@
 echo $(curl -k -XPOST --header 'Content-type: application/json' \
     --header "X-Auth-Token: $2" \
     --header "X-Auth-UserId: $3" \
-    -d '{"tracker":{"id":'$4'},"values_by_field":'$5'}' \
+    -d '{"tracker":{"id":'$4'},"values_by_field":'"$5"'}' \
     https://$1/api/artifacts)
