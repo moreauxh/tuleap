ef22f1cae880065a51afa5754c4f80c1bf1f6efe
Humbert <humbert.moreaux@enalean.com>
2017-01-19 14:15:33 +0100

    fix generate_token

--- actions/generate_token.sh
+++ actions/generate_token.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 payload='{"username":'$2', "password": '$3'}'
 
-echo $(curl -k -X POST -H -d "$payload" https://$1/api/artifacts)
+echo $(curl -k -X POST -H -d \'$payload\' https://$1/api/artifacts)
