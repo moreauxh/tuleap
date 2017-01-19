2b6b25517334a04c8cea718264c3820b5618f002
Humbert <humbert.moreaux@enalean.com>
2017-01-19 10:18:10 +0100

    show_project action update

--- actions/show_projects.sh
+++ actions/show_projects.sh
@@ -1,5 +1,2 @@
-#!/usr/bin/env bash
-
-RESULT=$(curl -k -H "X-Auth-Token: ${ST2_ACTION_AUTH_TOKEN}" $1:443/api/projects)
-
-echo ${RESULT}
+#!/bin/bash
+echo $(curl -k -H "X-Auth-Token: ${ST2_ACTION_AUTH_TOKEN}" $1:443/api/projects)
