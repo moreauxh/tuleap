d81582929a7209f171b7db9c22fbbb884df8681d
Humbert <humbert.moreaux@enalean.com>
2017-01-19 13:17:57 +0100

    curl fix

--- actions/add_artifact.sh
+++ actions/add_artifact.sh
@@ -4,4 +4,4 @@ payload='{
     "values_by_field": '$5'
 }'
 
-echo $(curl -k -X POST -H "X-Auth-Token: $2" -H "X-Auth-UserId: $3" -d \'$payload\' https://$1/api/artifacts)
+echo $(curl -k -X POST -H "X-Auth-Token: $2" -H "X-Auth-UserId: $3" -d "$payload" https://$1/api/artifacts)
