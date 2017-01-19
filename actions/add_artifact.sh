3b20a8db83cdcc0b1e48ca8ae21ec6a1e592a9c5
Humbert <humbert.moreaux@enalean.com>
2017-01-19 10:49:57 +0100

    new add_artifact actionx

---
+++ actions/add_artifact.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+payload = '{
+    "tracker": {"id" : '$2'},
+    "values": '$3'
+}'
+
+echo $(curl -k -H "X-Auth-Token: ${ST2_ACTION_AUTH_TOKEN}"  POST -d 'payload='$payload $1:443/api/artifacts)
