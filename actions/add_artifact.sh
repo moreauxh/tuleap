586bfe0b12237e5b7128b25c2867151fd8748a9b
Humbert <humbert.moreaux@enalean.com>
2017-01-19 12:59:39 +0100

    add user_id

--- actions/add_artifact.sh
+++ actions/add_artifact.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 payload = '{
-    "tracker": {"id" : '$3'},
-    "values_by_field": '$4'
+    "tracker": {"id" : '$4'},
+    "values_by_field": '$5'
 }'
 
-echo $(curl -k -X POST -H "X-Auth-Token: $2" -d \'$payload\' https://$1/api/artifacts)
+echo $(curl -k -X POST -H "X-Auth-Token: $2" -H "X-Auth-UserId: $3" -d \'$payload\' https://$1/api/artifacts)
